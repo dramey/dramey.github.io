09c95717146663e79d5ba18bcbe83527d7518c62
Deb Ramey <debramey.dr@gmail.com>
2016-09-29 12:33:47 -0400

    Added share, clear, and input restrictions to my calculator

--- tipCalc.js
+++ tipCalc.js
@@ -1,14 +1,21 @@
 $(function () {
+
+// I am declaring my variables
 var amountOfBill = ""
 var tipPercent = ""
 var totalOfTip = ""
 var totalBillIfSplit = ""
-
+// This is my calculate on the click of my calculate button
 $("#add").click(function () {
         amountOfBill = $("#amountOfBill").val();
         tipAmount = $("#tipPercent").find(":selected").val();
         amountInParty = $("#amountInParty").val();
-        
+
+        if(amountOfBill.toString().length > 6){
+    window.alert("Please Enter a Valid Amount of Your Bill");
+    return;
+        }
+        // these if/else statements will check to see if certain things apply before continuing on through
           if (amountOfBill === "") {
               window.alert("The Bill Amount Must Be Entered!")
           } else if (tipAmount == 0) {
@@ -17,7 +24,7 @@ $("#add").click(function () {
             window.alert("All fields Must be Entered");
 
         } else {
-
+// The below will do math if the above have all been 
             totalOfTip = (amountOfBill * tipAmount).toFixed(2);
             totalTipEach = ((totalOfTip / amountInParty) * 100/100);
             
@@ -28,15 +35,19 @@ $("#add").click(function () {
                 .append("<center>" + "Tip Per Person:" +  "$" + totalTipEach + "</center>" + "<br />")
                 .append ("<p>" + "Total Bill per Person:" + "$" + totalBillIfSplit + "</center>" + "<br />" + "</p>")
                 $("#showAmounts").append(div);
+                
     
     };
+    // this clears my page on click of my clear button
+$("#zeroOut").click(function () {
+    location.reload();
 
-    // $("#showAmounts").val("");
-
+   
 });
 });
+});
 
-// STOP
+
 
 
 
