030e946a997daad460c26c036cfada6892c19e6c
Deb Ramey <debramey.dr@gmail.com>
2016-10-02 08:57:52 -0400

    Fixed my Maply app

--- maply.js
+++ maply.js
@@ -2,14 +2,34 @@ $(function () {
     $("#addLocation").click(function () {
         var address = $("#address").val();
         $("#address").val("");
-        
-    var map = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCm9yzZC060tdk-OB0BeGP3jD5THIGLoXo&callback=initMap";
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
-        });
-      }
+
+        var googleUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyCm9yzZC060tdk-OB0BeGP3jD5THIGLoXo";
+        $.ajax(googleUrl, { dataType: "json" }).done(function (data) {
+            console.log(data);
+            city = data.results[0].address_components[1].long_name
+            state = data.results[0].address_components[3].short_name
+            placeLatitude = data.results[0].geometry.location.lat
+            placeLongitude = data.results[0].geometry.location.lng
+     var div = $("<div></div>");
+    div.append("<h4>" + city + " " + state + "</h4>")
+    .append("</br >")
+      $(".infoCard").html(div);
+      $("#map").html(initMap); 
+});
     });
 });
+function initMap() {
+        var map = new google.maps.Map(document.getElementById("map"), {
+          zoom: 10,
+          center: {lat: +placeLatitude, lng: +placeLongitude},
+        });
+        var marker = new google.maps.Marker({
+          position: {lat: +placeLatitude, lng: +placeLongitude},
+          map: map
+        });
+}
 
+var placeLatitude = "";
+var placeLongitude = "";
+var city = "";
+var state = "";
